c59392c7ff8383e46042a5ef5d143c99a7acfba6
Zhe Zhang <zhangzhe0177@gmail.com>
2026-01-11 22:45:26 +0800

    update: mac80211.uc bands_order reverse

--- prepare.sh
+++ prepare.sh
@@ -8,6 +8,8 @@ if [ -d "immortalwrt" ]; then
     echo "repo dir exists"
     cd immortalwrt
     git pull || { echo "git pull failed"; exit 1; }
+    git reset --hard HEAD
+    git clean -fd
 else
     echo "repo dir not exists"
     git clone -b openwrt-24.10 --single-branch --filter=blob:none "https://github.com/immortalwrt/immortalwrt" || { echo "git clone failed"; exit 1; }
@@ -22,6 +24,14 @@ echo "src-git qmodem https://github.com/FUjr/QModem.git;main" >> feeds.conf
 echo "update files"
 rm -rf files
 cp -r ../files .
+
+# WLAN Compatibility Fix
+cp package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc ./files/lib/wifi/mac80211.uc
+sed -i 's/const bands_order = \[ "6G", "5G", "2G" \];/const bands_order = [ "2G", "5G", "6G" ];/' ./files/lib/wifi/mac80211.uc
+echo "diff lib/wifi/mac80211.uc with builder repo:"
+diff ../files/lib/wifi/mac80211.uc ./files/lib/wifi/mac80211.uc
+echo "diff lib/wifi/mac80211.uc with immortalwrt repo:"
+diff package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc ./files/lib/wifi/mac80211.uc
 
 echo "update feeds"
 ./scripts/feeds update -a || { echo "update feeds failed"; exit 1; }
